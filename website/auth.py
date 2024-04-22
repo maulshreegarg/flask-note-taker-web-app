8e0aec13174ef385a4df42fc5fd1c30d2b18efa2
maulshreegarg <98210535+maulshreegarg@users.noreply.github.com>
2024-04-22 21:38:15 +0530

    Initial commit

---
+++ website/auth.py
@@ -0,0 +1,12 @@
+from flask import Blueprint,render_template
+auth=Blueprint('auth',__name__)
+@auth.route('/login')
+def login():
+    return render_template("login.html",text="att1")
+@auth.route('/logout')
+def logout():
+    return '<p>logout</p>'
+@auth.route('/signup')
+def signup():
+  return render_template("signup.html")
+
